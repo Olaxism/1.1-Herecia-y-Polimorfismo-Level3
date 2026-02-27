808abebc1ad41ab34f437f2f4b6ec542f8947f21
Olaxism <arq.medrano@hotmail.com>
2026-02-27 11:19:01 +0100

    feat: Added methods for Total Score and Total Price for Basketball News

--- src/Nivel3/BasketballNews.java
+++ src/Nivel3/BasketballNews.java
@@ -19,6 +19,33 @@ public class BasketballNews extends News {
     public String getClub() {
         return club;
     }
+    public int getScore() {
+
+        int totalScore = 4;
+        if (competition.equals("Euroliga")) {
+            totalScore += 3;
+        }
+        if (competition.equals("ACB")) {
+            totalScore += 2;
+        }
+        if (club.equals("Madrid") || club.equals("Barça")) {
+            totalScore += 1;
+        }
+
+        return totalScore;
+    }
+
+    public double getPrice() {
+
+        double totalPrice = 250;
+        if (competition.equals("Euroliga")) {
+            totalPrice += 75;
+        }
+        if (club.equals("Madrid") || club.equals("Barça")) {
+            totalPrice += 75;
+        }
+        return totalPrice;
+    }
 
     @Override
     public String toString() {
