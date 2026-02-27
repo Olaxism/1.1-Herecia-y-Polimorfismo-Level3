9bf39198ea257ca36fa9823e1d37af606d702eb9
Olaxism <arq.medrano@hotmail.com>
2026-02-27 11:08:54 +0100

    feat: Added methods for Total Score and Total Price for FootBall News

--- src/Nivel3/FootballNews.java
+++ src/Nivel3/FootballNews.java
@@ -25,6 +25,39 @@ public class FootballNews extends News {
             return player;
         }
 
+        public int getScore() {
+
+            int totalScore = 5;
+            if (competition.equals("Liga de Champions")) {
+                totalScore += 3;
+            }
+            if (competition.equals("Liga")) {
+                totalScore += 1;
+            }
+            if (club.equals("Madrid") || club.equals("Barça")) {
+                totalScore += 1;
+            }
+            if (player.equals("Ferran Torres") || player.equals("Benzema")) {
+                totalScore += 1;
+            }
+            return totalScore;
+        }
+
+        public double getPrice() {
+
+            double totalPrice = 300;
+            if (competition.equals("Liga de Champions")) {
+                totalPrice += 100;
+            }
+            if (club.equals("Madrid") || club.equals("Barça")) {
+                totalPrice += 100;
+            }
+            if (player.equals("Ferran Torres") || player.equals("Benzema")) {
+                totalPrice += 50;
+            }
+            return totalPrice;
+        }
+
     @Override
     public String toString() {
         return "FootballNews{" +
