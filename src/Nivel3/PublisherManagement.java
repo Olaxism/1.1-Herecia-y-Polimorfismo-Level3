535c257e122f53ce288d06f462dac5d305e73b58
Olaxism <arq.medrano@hotmail.com>
2026-03-04 12:16:34 +0100

    Added options 4, 5, 6, and last one 7 to the main menu of exercise

--- src/Nivel3/PublisherManagement.java
+++ src/Nivel3/PublisherManagement.java
@@ -1,8 +1,6 @@
 package Nivel3;
 
-import Nivel3.classes.FootballNews;
-import Nivel3.classes.Redactor;
-import Nivel3.classes.SportsPublisher;
+import Nivel3.classes.*;
 
 import java.util.Scanner;
 
@@ -50,7 +48,6 @@ public class PublisherManagement {
         Redactor foundRedactor = null;
 
         do {
-
             System.out.println("\nIntroduzca el DNI del Redactor quien cubrirá la noticia");
             String dni = sc.next();
 
@@ -93,7 +90,210 @@ public class PublisherManagement {
             FootballNews newFN = new FootballNews( title, competition, club, player);
             foundRedactor.addToMyNews(newFN);
 
-            System.out.println("Noticia agregada a Redactor: " + foundRedactor.getName();
+            case 2:
+                System.out.println("Competición: ");
+                String competition2 = sc.next();
+
+                System.out.println("Club: ");
+                String club2 = sc.next();
+
+            BasketballNews newBN = new BasketballNews (title, competition2, club2);
+            foundRedactor.addToMyNews(newBN);
+
+            case 3:
+                System.out.println("Competición: ");
+                String competition3 = sc.next();
+
+                System.out.println("Jugadores: ");
+                String players = sc.next();
+
+            TenisNews newTN = new TenisNews (title, competition3, players);
+            foundRedactor.addToMyNews(newTN);
+
+            case 4:
+                System.out.println("Escuderia: ");
+                String squad = sc.next();
+
+            F1News newF1N = new F1News (title, squad);
+            foundRedactor.addToMyNews(newF1N);
+
+            case 5:
+                System.out.println("Equipo: ");
+                String team = sc.next();
+
+            MotoNews newMN = new MotoNews (title, team);
+            foundRedactor.addToMyNews(newMN);
+
         }
+
+        System.out.println("Noticia agregada a Redactor: " + foundRedactor.getName());
+    }
+
+    private static void removeNewsFromRedactor() {
+        System.out.println("---Eliminar Noticia de un Redactor---");
+
+        Redactor foundRedactor = null;
+
+        do {
+            System.out.println("\nIntroduzca el DNI del Redactor");
+            String dni = sc.next();
+
+            for (int i = 0; i < sp.getRedactors().size(); i++) {
+                if (sp.getRedactors().get(i).getDni().equalsIgnoreCase(dni)) {
+                    foundRedactor = sp.getRedactors().get(i);
+                    System.out.println("Redactor Encontrado");
+                    break;
+                }
+            }
+            if (foundRedactor == null) {
+                System.out.println("Este DNI no pertenece a ninguno de nuestro redactores actuales");
+            }
+
+        }while (foundRedactor == null);
+
+        News foundNews = null;
+
+        do {
+            System.out.println("\n Por favor, Introduzca el titulo de la noticia que desea eliminar: ");
+            String title = sc.next();
+
+            for (News n : foundRedactor.getMyNews()) {
+                if ( n.getTitle().equalsIgnoreCase(title)) {
+                     foundNews = n;
+                     break;
+                }
+            }
+
+            if (foundNews == null) {
+
+                System.out.println("No se encontró ninguna noticia con este titular.");
+                System.out.println("Intentelo de nuevo.");
+            }
+
+        } while (foundNews == null);
+
+        foundRedactor.removeNews(foundNews.getTitle());
+        System.out.println("\nNoticia con titulo " + foundNews.getTitle() +
+                " ha sido eliminada del Redactor: " + foundRedactor.getName());
+
+    }
+
+    private static void showRedactorAllNews() {
+        System.out.println("---Mostrar todas las noticias de un Redactor---");
+
+        Redactor foundRedactor = null;
+
+        do {
+            System.out.println("\nIntroduzca el DNI del Redactor");
+            String dni = sc.next();
+
+            for (int i = 0; i < sp.getRedactors().size(); i++) {
+                if (sp.getRedactors().get(i).getDni().equalsIgnoreCase(dni)) {
+                    foundRedactor = sp.getRedactors().get(i);
+                    System.out.println("Redactor Encontrado");
+                    break;
+                }
+            }
+            if (foundRedactor == null) {
+                System.out.println("Este DNI no pertenece a ninguno de nuestro redactores actuales");
+            }
+        }    while (foundRedactor == null);
+
+        System.out.println("Estas son todas las noticias que estan bajo el Redactor: " + foundRedactor.getName());
+        System.out.println(foundRedactor.getMyNews());
+
+        }
+
+    private static void totalScoreNews() {
+        System.out.println("---Mostrar puntuación de una noticia---");
+
+        Redactor foundRedactor = null;
+
+        do {
+            System.out.println("\nIntroduzca el DNI del Redactor que tiene la noticia");
+            String dni = sc.next();
+
+            for (int i = 0; i < sp.getRedactors().size(); i++) {
+                if (sp.getRedactors().get(i).getDni().equalsIgnoreCase(dni)) {
+                    foundRedactor = sp.getRedactors().get(i);
+                    System.out.println("Redactor Encontrado");
+                    break;
+                }
+            }
+            if (foundRedactor == null) {
+                System.out.println("Este DNI no pertenece a ninguno de nuestro redactores actuales");
+            }
+        }    while (foundRedactor == null);
+
+        News foundNews = null;
+
+        do {
+            System.out.println("\n Por favor, Introduzca el titulo de la noticia: ");
+            String title = sc.next();
+
+            for (News n : foundRedactor.getMyNews()) {
+                if ( n.getTitle().equalsIgnoreCase(title)) {
+                    foundNews = n;
+                    break;
+                }
+            }
+
+            if (foundNews == null) {
+
+                System.out.println("No se encontró ninguna noticia con este titular.");
+                System.out.println("Intentelo de nuevo.");
+            }
+
+        } while (foundNews == null);
+
+        System.out.println("La puntuacion total de la noticia con titulo: " + foundNews.getTitle() + " es: ");
+        System.out.println(foundNews.getScore());
+
+    }
+
+    private static void totalPriceNews() {
+        System.out.println("---Mostrar precio de una noticia---");
+
+        Redactor foundRedactor = null;
+
+        do {
+            System.out.println("\nIntroduzca el DNI del Redactor que tiene la noticia");
+            String dni = sc.next();
+
+            for (int i = 0; i < sp.getRedactors().size(); i++) {
+                if (sp.getRedactors().get(i).getDni().equalsIgnoreCase(dni)) {
+                    foundRedactor = sp.getRedactors().get(i);
+                    System.out.println("Redactor Encontrado");
+                    break;
+                }
+            }
+            if (foundRedactor == null) {
+                System.out.println("Este DNI no pertenece a ninguno de nuestro redactores actuales");
+            }
+        }    while (foundRedactor == null);
+
+        News foundNews = null;
+
+        do {
+            System.out.println("\n Por favor, Introduzca el titulo de la noticia: ");
+            String title = sc.next();
+
+            for (News n : foundRedactor.getMyNews()) {
+                if ( n.getTitle().equalsIgnoreCase(title)) {
+                    foundNews = n;
+                    break;
+                }
+            }
+
+            if (foundNews == null) {
+
+                System.out.println("No se encontró ninguna noticia con este titular.");
+                System.out.println("Intentelo de nuevo.");
+            }
+
+        } while (foundNews == null);
+
+        System.out.println("El precio total de la noticia con titulo: " + foundNews.getTitle() + " es: ");
+        System.out.println(foundNews.getPrice());
     }
 }
